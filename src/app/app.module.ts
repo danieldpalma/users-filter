fef36109ce8239a8c5e10fe93545e023eaaf473c
danieldpalma <danielpalma232@gmail.com>
2025-07-11 15:25:08 -0300

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponentModule } from './components/components.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ComponentModule],
+  imports: [BrowserModule, ComponentModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
