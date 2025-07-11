fc51b6f57130e26335c2d813032394d8d23849db
danieldpalma <danielpalma232@gmail.com>
2025-07-11 17:27:42 -0300

    added layout user details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentModule } from './components/components.module';
+import { ComponentsModule } from './components/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ComponentModule, BrowserAnimationsModule],
+  imports: [BrowserModule, ComponentsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
