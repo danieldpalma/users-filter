3c640b9bbbccca2835d1b227b3746aadac09f128
danieldpalma <danielpalma232@gmail.com>
2025-08-15 23:02:52 -0300

    added users list component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,10 +6,11 @@ import { AngularMaterialModule } from '../angular-material/angular-material.modu
 
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  declarations: [UserDetailsComponent, FilterComponent],
+  declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
   imports: [AngularMaterialModule, FormsModule, CommonModule],
-  exports: [UserDetailsComponent, FilterComponent],
+  exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule {}
