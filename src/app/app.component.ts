461cf248a286e794464ca146a014eb229d9dbaba
danieldpalma <danielpalma232@gmail.com>
2025-08-19 17:40:49 -0300

    implemented onInit to usersList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  usersList: IUser[] = [];
 
   onUserSelected(user: IUser) {
     this.showUserDetails = true;
     this.userSelected = user;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 3000);
   }
 }
